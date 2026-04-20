68a2c2f16913ca67b1cd0d92a079f28d96794d2b
Mamii868 <marcmotenii@gmail.com>
2026-04-20 15:20:21 -0500

    Add line numbers

--- src/main/java/com/pluralsight/BedtimeStoriesApplication.java
+++ src/main/java/com/pluralsight/BedtimeStoriesApplication.java
@@ -6,23 +6,26 @@ import java.util.Scanner;
 
 public class BedtimeStoriesApplication {
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        try{
-            System.out.println("Enter the name of a story: ");
+        try {
+            System.out.print("Enter the name of a story: ");
             String storyName = scanner.nextLine().toLowerCase() + ".txt";
 
-            if(storyName.contains(" ")) {
+            if (storyName.contains(" ")) {
                 storyName = storyName.replace(" ", "_");
             }
 
-            FileReader fileReader = new FileReader(storyName);
+            FileReader fileReader = new FileReader("src/main/resources/" + storyName);
 
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             String currentLine;
+            int currentLineNumber = 1;
 
-            while((currentLine = bufferedReader.readLine()) != null) {
-                System.out.println(currentLine);
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                System.out.println(currentLineNumber + ". " + currentLine);
+                currentLineNumber++;
             }
 
         } catch (Exception e) {
