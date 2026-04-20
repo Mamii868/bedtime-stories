e866cc1af7551d9087cc8bc813331d54c0df0be3
Mamii868 <marcmotenii@gmail.com>
2026-04-20 15:13:53 -0500

    Add file reader

--- src/main/java/com/pluralsight/BedtimeStoriesApplication.java
+++ src/main/java/com/pluralsight/BedtimeStoriesApplication.java
@@ -1,7 +1,30 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class BedtimeStoriesApplication {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        try{
+            System.out.println("Enter the name of a story: ");
+            String storyName = scanner.nextLine().toLowerCase() + ".txt";
+
+            FileReader fileReader = new FileReader(storyName);
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String currentLine;
+
+            while((currentLine = bufferedReader.readLine()) != null) {
+                System.out.println(currentLine);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error occurred");
+        }
+
 
     }
 }
